1a061aec6beac6b2a69d7c05f73417a969d4e3c1
Carolina de Senne Garcia <desennecarol@gmail.com>
2020-03-17 19:23:58 -0300

    Add more functions to parser

--- parser/parser.py
+++ parser/parser.py
@@ -113,11 +113,9 @@ class Parser():
 
     def p_primary_expression(self, p):
         """
-        primary_expression : ID
-                           | INT_CONST
-                           | FLOAT_CONST
-                           | CHAR_CONST
-                           | STRING_LITERAL
+        primary_expression : identifier
+                           | constant
+                           | string
                            | LPAREN expression RPAREN
         """
         if len(p)==2: # (id,constant and string)
@@ -125,5 +123,180 @@ class Parser():
         else: # (expression)
             p[0] = p[2]
 
+    def p_constant(self, p):
+        """
+        constant : integer_constant
+                 | character_constant
+                 | floating_constant
+        """
+        p[0] = p[1]
 
+    def p_expression(self, p):
+        """
+        expression : assignment_expression
+                   | expression COMMA assignment_expression 
+        """
+        if len(p)==2:
+            p[0] = p[1]
+        else:
+            p[0] = (',',p[1],p[3])
 
+    def p_assignment_expression(self, p):
+        """
+        assignment_expression : binary_expression
+                              | unary_expression assignment_operator assignment_expression
+        """
+        if len(p)==2:
+            p[0] = p[1]
+        else:
+            p[0] = (p[2],p[1],p[3])
+
+    def p_assignment_operator(self, p):
+        """
+        assignment_operator : EQUALS
+                            | TIMESEQUAL
+                            | DIVEQUAL
+                            | MODEQUAL
+                            | PLUEQUAL
+                            | MINUSEQUAL
+        """
+        p[0] = p[1]
+
+    def p_unary_operator(self, p):
+        """
+        unary_operator : ADDRESS
+                       | TIMES
+                       | PLUS
+                       | MINUS
+                       | NOT
+        """
+        p[0] = p[1]
+
+    def p_parameter_list(self, p):
+        """
+        parameter_list : parameter_declaration
+                       | parameter_list COMMA parameter_declaration
+        """
+        if len(p)==2:
+            p[0] = [p[1]]
+        else:
+            p[0] = p[1] + [p[3]]
+
+    def p_parameter_declaration(self, p):
+        """
+        parameter_declaration : {<type_specifier>} declarator
+        """
+        p[0] = (p[1],p[2])
+        #TODO
+
+    def p_declaration(self, p):
+        """
+        declaration : {type_specifier} {init_declarator}* SEMI
+        """
+        #TODO
+
+    def p_init_declarator(self, p):
+        """
+        init_declarator : declarator
+                        | declarator EQUALS initializer
+        """
+        if len(p)==2:
+            p[0] = p[1]
+        else:
+            p[0] = ('=',p[1],p[3])
+
+    def p_initializer(self, p):
+        """
+        initializer : assignment_expression
+                    | { initializer_list }
+                    | { initializer_list , }
+        """
+        #TODO
+
+    def p_initializer_list(self, p):
+        """
+        initializer_list : initializer
+                         | initializer_list COMMA initializer
+        """
+        if len(p)==2:
+            p[0] = [p[1]]
+        else:
+            p[0] = p[1] + [p[3]]
+
+    def p_compound_statement(self, p):
+        """
+        { {<declaration>}* {<statement>}* }
+        """
+        #TODO
+
+    def p_statement(self, p):
+        """
+        statement : expression_statement
+                  | compound_statement
+                  | selection_statement
+                  | iteration_statement
+                  | jump_statement
+                  | assert_statement
+                  | print_statement
+                  | read_statement
+        """
+        p[0] = p[1]
+
+    def p_expression_statement(self, p):
+        """
+        expression_statement : {<expression>}? SEMI
+        """
+        #TODO
+
+    def p_selection_statement(self, p):
+        """
+        selection_statement : IF LPAREN expression RPAREN statement
+                            | IF LPAREN expression RPAREN statement ELSE statement
+
+        """
+        #TODO
+
+    def p_iteration_statement(self, p):
+        """
+        iteration_statement : WHILE LPAREN expression RPAREN statement
+                            | FOR LPAREN {expression}? SEMI {expression}? SEMI {expression}? RPAREN statement
+        """
+        #TODO
+
+    def p_jump_statement(self, p):
+        """
+        jump_statement : BREAK SEMI
+                       | RETURN {expression}? SEMI
+        """
+        #TODO
+
+    def p_assert_statement(self, p):
+        """
+        assert_statement : ASSERT expression SEMI
+        """
+        p[0] = ('assert',p[2])
+
+    def p_print_statement(self, p):
+        """
+        print_statement : PRINT LPAREN <expression>* RPAREN SEMI
+        """
+        #TODO
+
+    def p_read_statement(self, p):
+        """
+        read_statement : READ LPAREN <declarator>+ RPAREN SEMI
+        """
+        #TODO
+    
+    def p_error (p):
+        if p:
+            print("Error near the symbol %s" % p.value)
+        else:
+            print("Error at the end of input")
+
+    precedence = (
+    ('left', 'PLUS'),
+    ('left', 'TIMES')
+    ) #TODO are there others?
+
+    parser = yacc.yacc(write_tables=False)
