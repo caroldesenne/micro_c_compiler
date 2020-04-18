d5332576ada29a49d0503f365a548b651c1966ed
Carolina de Senne Garcia <desennecarol@gmail.com>
2020-04-18 19:36:44 -0300

    Specify types and corresponding operations

--- parser/uctype.py
+++ parser/uctype.py
@@ -3,39 +3,61 @@ class uCType(object):
     Class that represents a type in the uC language.  Types 
     are declared as singleton instances of this type.
     '''
-    def __init__(self, name, bin_ops=set(), un_ops=set()):
-        '''
-        You must implement yourself and figure out what to store.
-        '''
+    def __init__(self, name, bin_ops=set(), un_ops=set(), bool_ops=set(), as_ops=set()):
         self.name = name
         self.bin_ops = bin_ops
         self.un_ops = un_ops
+        self.bool_ops = bool_ops
+        self.assign_ops = as_ops
 
 
 # Create specific instances of types. You will need to add
 # appropriate arguments depending on your definition of uCType
-int_type = uCType("int",
-    set(('PLUS', 'MINUS', 'TIMES', 'DIVIDE',
-         'LE', 'LT', 'EQ', 'NE', 'GT', 'GE')),
-    set(('PLUS', 'MINUS')),
-    )
-float_type = uCType("float",
-    set(('PLUS', 'MINUS', 'TIMES', 'DIVIDE',
-         'LE', 'LT', 'EQ', 'NE', 'GT', 'GE')),
-    set(('PLUS', 'MINUS')),
-    )
-char_type = uCType("char",
-    set(('PLUS',)),
-    set(),
-    )
-string_type = uCType("string",
-    set(('PLUS',)),
-    set(),
-    )
-boolean_type = uCType("bool",
-    set(('AND', 'OR', 'EQ', 'NE')),
-    set(('NOT',))
-    )
-# In your type checking code, you will need to reference the
-# above type objects.   Think of how you will want to access
-# them.
+int_type = uCType(name     = "int",
+                  bin_ops  = {'+', '-', '*', '/','%','==','!=','<','>','<=','>='},
+                  un_ops   = {'-','+','--','++','p--','p++','*','&'},
+                  bool_ops = {'==','!=','<','>','<=','>='},
+                  as_ops   = {'=','+=','-=','*=','/=','%='}
+                 )
+
+float_type = uCType(name     = "float",
+                    bin_ops  = {'+', '-', '*', '/','%','==','!=','<','>','<=','>='},
+                    un_ops   = {'-','+','*','&'},
+                    bool_ops = {'==','!=','<','>','<=','>='},
+                    as_ops   = {'=','+=','-=','*=','/=','%='}
+                   )
+
+boolean_type = uCType(name     = "bool",
+                      bin_ops  = {'==','!=','&&','||'},
+                      un_ops   = {'!','*','&'},
+                      bool_ops = {'==','!=','&&','||'},
+                      as_ops   = {}
+                     )
+
+char_type = uCType(name     = "char",
+                   bin_ops  = {'==','!='},
+                   un_ops   = {'*','&'},
+                   bool_ops = {'==','!='}, # TODO: sao so esses?
+                   as_ops   = {}
+                  )
+
+string_type = uCType(name     = "string",
+                     bin_ops  = {'==','!='},
+                     un_ops   = {},
+                     bool_ops = {'==','!='},
+                     as_ops   = {}
+                    )
+
+array_type = uCType(name = "array",
+                    bin_ops  = {'==','!='},
+                    un_ops   = {'*','&'},
+                    bool_ops = {'==','!='},
+                    as_ops   = {}
+                    )
+# TODO pointer
+pointer_type = uCType(name = "pointer",
+                      bin_ops  = {},
+                      un_ops   = {},
+                      bool_ops = {},
+                      as_ops   = {}
+                     )
