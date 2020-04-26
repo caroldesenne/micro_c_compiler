4663704496171bbc41aca59ed1a1dacda880bd43
Carolina de Senne Garcia <desennecarol@gmail.com>
2020-04-26 13:47:09 -0300

    Avoid code repetition

--- parser/generate.py
+++ parser/generate.py
@@ -3,6 +3,7 @@ import uctype
 from parser import Parser
 from collections import defaultdict
 from uc_ast import *
+from check import *
 
 binary_ops = {
     '+': 'add',
@@ -16,74 +17,6 @@ unary_ops = {
     '+': 'uadd',
     '-': 'uneg',
 }
-
-class NodeVisitor(object):
-    """ A base NodeVisitor class for visiting uc_ast nodes.
-        Subclass it and define your own visit_XXX methods, where
-        XXX is the class name you want to visit with these
-        methods.
-
-        For example:
-
-        class ConstantVisitor(NodeVisitor):
-            def __init__(self):
-                self.values = []
-
-            def visit_Constant(self, node):
-                self.values.append(node.value)
-
-        Creates a list of values of all the constant nodes
-        encountered below the given node. To use it:
-
-        cv = ConstantVisitor()
-        cv.visit(node)
-
-        Notes:
-
-        *   generic_visit() will be called for AST nodes for which
-            no visit_XXX method was defined.
-        *   The children of nodes for which a visit_XXX was
-            defined will not be visited - if you need this, call
-            generic_visit() on the node.
-            You can use:
-                NodeVisitor.generic_visit(self, node)
-        *   Modeled after Python's own AST visiting facilities
-            (the ast module of Python 3.0)
-    """
-
-    _method_cache = None
-
-    def visit(self, node):
-        """ Visit a node.
-        """
-        if self._method_cache is None:
-            self._method_cache = {}
-
-        visitor = self._method_cache.get(node.__class__.__name__, None)
-        if visitor is None:
-            method = 'visit_' + node.__class__.__name__
-            print(method)
-            visitor = getattr(self, method, self.generic_visit)
-            self._method_cache[node.__class__.__name__] = visitor
-
-        return visitor(node)
-
-    def generic_visit(self, node):
-        """ Called if no explicit visitor function exists for a
-            node. Implements preorder visiting of the node.
-        """
-        for c in node:
-            self.visit(c)
-
-def getInnerMostType(node):
-    td = node
-    while td and (not isinstance(td,Type)):
-        td = td.type
-    return td
-
-def getBasicType(node):
-    t = getInnerMostType(node)
-    return t.names[0]
 
 class GenerateCode(NodeVisitor):
     '''
